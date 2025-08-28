8fe084674b97069965fec08d97192f53863e7798
Risabh Yadav <rashiyadav1505@gmail.com>
2025-08-28 15:13:46 +0530

    further3

--- src/components/Navbar/nav.jsx
+++ src/components/Navbar/nav.jsx
@@ -6,10 +6,10 @@ const Navigation = () => {
       <div className="main-nav-container w-full h-[9vh] flex justify-between items-center px-[7vw] bg-black">
         <img className="w-[8em]" src={Mysignimage} alt="logo" />
         <button>
-          <div className="space-y-1.5 ">
-            <span className="block h-1 w-7 bg-white rounded-2xl"></span>
-            <span className="block h-1 w-7 bg-white rounded-2xl"></span>
-            <span className="block h-1 w-7 bg-white rounded-2xl"></span>
+          <div className="space-y-1 ">
+            <span className="block h-[.2em] w-7 bg-white rounded-2xl"></span>
+            <span className="block h-[.2em] w-7 bg-white rounded-2xl"></span>
+            <span className="block h-[.2em] w-7 bg-white rounded-2xl"></span>
           </div>
         </button>
       </div>
