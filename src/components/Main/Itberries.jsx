efe1594ec3bcf0a70a140bc724d60c8d7fde64e5
Risabh Yadav <rashiyadav1505@gmail.com>
2025-08-30 00:52:15 +0530

    testing further

--- src/components/Main/Itberries.jsx
+++ src/components/Main/Itberries.jsx
@@ -5,7 +5,7 @@ const Itberries = () => {
   return (
     <>
       <div
-        className="bg-[#0f0e0e] sm:bg-[length:25vw_50vh] sm:h-[35vh] sm:bg-right sm:items-start flex flex-col sm:pl-40 sm:pr-30 pl-[12vw] sm:pt-7 pr-[12vw] pt-[5vh] pb-[5vh] justify-between items-center h-[57vh] w-full bg-[length:80vw_45vh] bg-top bg-no-repeat"
+        className="bg-[#0f0e0e] sm:bg-[length:25vw_50vh] sm:h-[35vh] sm:bg-right sm:items-start flex flex-col sm:pl-40 sm:pr-30 pl-[12vw] sm:pt-7 pr-[12vw] py-[4vh] justify-between items-center h-[45vh] w-full bg-[length:75vw_40vh] bg-top bg-no-repeat"
         style={{ backgroundImage: `url(${Itberriesimage})` }}
       >
         <h3 className="text-white text-[1.5em] font-semibold font-mono">
